f0bf611826e9e2bb685ac4fbb21a32e10bee5f6a
Jay Valani <jay.valani.tatvasoft@gmail.com>
2022-06-27 18:56:11 +0530

    schedule screen & other minor changes

---
+++ src/screens/cancle-screen/CancleScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CancleScreen = () => {
+  return (
+    <View>
+      <Text>CancleScreen</Text>
+    </View>
+  )
+}
+
+export default CancleScreen
+
+const styles = StyleSheet.create({})
